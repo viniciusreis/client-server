82e1d9ea9816d753a0146f6a476a54c29812b8ae
Vinicius Reis <6201339+viniciusreis@users.noreply.github.com>
2024-05-30 03:07:51 -0300

    Add a blocking connection

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,44 @@
-#include <Networking/Base.h>
+#include "boost/asio/connect.hpp"
+#include "boost/asio/error.hpp"
+#include <array>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio.hpp>
+#include <exception>
+#include <iostream>
 
 int main() {
 
-  processInput();
+  using namespace boost::asio;
+  using boost::asio::io_context;
+  using boost::asio::ip::tcp;
+
+  try {
+    std::cout << "Connecting to local host...\n";
+    io_context context;
+    tcp::resolver resolver(context);
+    tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "1234");
+
+    tcp::socket socket(context);
+    connect(socket, endpoints);
+
+      std::cout << "Connected to server! Receiving message...\n";
+    while(true) {
+      std::array<char, 128> buf;
+      boost::system::error_code error;
+      size_t len = socket.read_some(buffer(buf), error);
+
+      if (error == error::eof) {
+        break;
+      } else if (error) {
+        throw boost::system::system_error(error);
+      }
+
+      std::cout.write(buf.data(), len);
+    }
+
+  } catch(std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,34 @@
+#include <iostream>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/write.hpp>
 #include <Networking/Base.h>
+#include <boost/asio.hpp>
 
 int main() {
-  processInput();
+
+  using namespace boost::asio;
+  using boost::asio::ip::tcp;
+  using boost::asio::io_context;
+
+  try {
+    std::cout << "Accepting connections on port 1234...\n";
+    io_context context;
+    tcp::acceptor acceptor(context, tcp::endpoint(tcp::v4(), 1234));
+
+    while(true) {
+      tcp::socket socket(context);
+      acceptor.accept(socket);
+
+      std::cout << "Client connected! Sending message...\n";
+      std::string msg = "Server is alive!\n";
+      boost::system::error_code error;
+
+      write(socket, buffer(msg), error);
+
+    }
+
+  } catch(std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
 }
